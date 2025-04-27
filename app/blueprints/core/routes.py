f74ed7718070f42799c68c4c1a6563eaeb28adb0
Sergio Godinho <nikdiksnk@gmail.com>
2025-04-27 14:31:32 -0300

    Fix profile page posts showing post in wrong order

--- app/blueprints/core/routes.py
+++ app/blueprints/core/routes.py
@@ -33,7 +33,7 @@ def index():
 @core.route('/profile/<string:username>', methods=['GET'])
 def profile(username):
     user = User.query.filter(User.username == username).first()
-    posts = Post.query.filter(Post.user.has(username=username))
+    posts = Post.query.filter(Post.user.has(username=username)).order_by(db.desc(Post.id_post))
 
     return render_template('core/profile.html', user=user, posts=posts)
 
