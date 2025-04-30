55603627ae74b101ee307105e45c6898c5b91fe3
Sergio Godinho <nikdiksnk@gmail.com>
2025-04-30 12:56:13 -0300

    Update profile update code

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -24,13 +24,13 @@ def login():
 
         user = User.query.filter(User.email == email).first()
         if user is None:
-            return 'Failed'
+            return 'Failed - User does not exist.'
 
         if bcrypt.check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('core.index'))
         else:
-            return 'Failed'
+            return 'Failed - Wrong password'
 
 
 @auth.route('/logout', methods=['GET'])
@@ -51,10 +51,10 @@ def sign_up():
         password = bcrypt.generate_password_hash(data['password'])
 
         if User.query.filter(User.email == email).first() is not None:
-            return 'Failed'
+            return 'Failed - Email is already registered'
 
         user = User(username=username, email=email,
-                    password=password, nickname=username)
+                    password=password, nickname=username, description='')
 
         db.session.add(user)
         db.session.commit()
@@ -70,18 +70,22 @@ def update():
     elif request.method == 'POST':
         nickname = request.form.get('nickname')
         email = request.form.get('email')
-        password = bcrypt.generate_password_hash(request.form.get('password'))
+        password = request.form.get('password')
+        new_password = request.form.get('new-password')
         description = request.form.get('desc')
-        print(type(current_user.id_user))
+
+        if not bcrypt.check_password_hash(current_user.password, password):
+            return 'Failed - Wrong current password'
 
         if User.query.filter(User.email == email).first() is not None and current_user.email != email:
-            return 'Failed'
+            return 'Failed - E-mail is already registered'
 
         user = db.session.get(User, current_user.id_user)
         user.nickname = nickname
         user.email = email
-        user.password = password
         user.description = description
+        if new_password != '':
+            user.password = bcrypt.generate_password_hash(new_password)
 
         db.session.commit()
 
